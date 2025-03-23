5dc5c934c2f6013192a2c5bdaa1676640f48d01b
vukasinz <127794519+vukasinz@users.noreply.github.com>
2025-03-24 00:10:22 +0100

    movement v1.2
    popravljen skok
    popravljen dash
    dash trail dodat

--- Assets/Scripts/player_mov.cs
+++ Assets/Scripts/player_mov.cs
@@ -4,71 +4,39 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class player_mov : MonoBehaviour
 {
+    [Header("Rigidbody")]
     public Rigidbody2D rb;
     public int direction;
+    public int oldDirection;
     public int directionPar;
+    [Header("Movement")]
     public int speed = 10;
+    public LayerMask groundLayer;
+    public Animator anim;
+    [Header("Jump")]
     public float jumpForce = 500f;
     public float fallMultiplier = 3f;
     public float lowJumpMultiplier = 2f;
-
-    float dashProgress = 0.7f;
     public bool isGrounded;
-    public bool canDash = true;
-    public bool isDashing = false;
-    public Animator anim;
+    [Header("Dash")]
+    public float _dashingVelocity = 15f;
+    public float _dashingTime = 0.5f;
+    public Vector2 _dashingDir;
+    public bool _isDashing;
+    public bool _canDash = true;
+    public TrailRenderer _trailRen;
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = this.GetComponentInChildren<Animator>();
+        _trailRen = GetComponentInChildren<TrailRenderer>();
     }
 
-    // pdate is called once per frame
-    private void FixedUpdate()
-    {
-        if(!isDashing)
-            rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
 
-        if (Mathf.Abs(rb.linearVelocityY) < 0.1f)
-            isGrounded = true;
-        else
-            isGrounded = false;
-      
-    }
-    public void Dash()
-    {
-        rb.AddForce(new Vector2(direction * 200f, 0), ForceMode2D.Force);
-        
-        isDashing = true;
-        canDash = false;
-        anim.SetTrigger("isDashing");
-        StartCoroutine("dashColor");
-    }
-
-    private void DashOnOff()
-    {
-        if (isDashing)
-        {
-            if (dashProgress > 0)
-                dashProgress -= Time.deltaTime;
-            if (dashProgress <= 0)
-            {
-                dashProgress = 0.7f;
-                isDashing = false;
-                rb.linearVelocityX = 0;
-            }
-        }
-    }
-    IEnumerator dashColor()
-    {
-        Color original = this.GetComponentInChildren<SpriteRenderer>().color;
-        Color target = original;
-        target.a = 0.5f;
-        this.GetComponentInChildren<SpriteRenderer>().color = target;
-        yield return new WaitForSeconds(0.5f);
-        this.GetComponentInChildren<SpriteRenderer>().color = original;
-    }
+   
     void Move()
     {
         directionPar = direction;
@@ -93,25 +61,57 @@ public class player_mov : MonoBehaviour
             rb.linearVelocity += Vector2.up * (lowJumpMultiplier - 1) * Time.deltaTime * Physics2D.gravity.y;
         }
     }
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(_dashingTime);
+        _isDashing = false;
+        _trailRen.emitting = false;
+    }
     void Update()
     {
-       BetterJump();
-        DashOnOff();
-        if (!canDash && isGrounded)
-            canDash = true;
+        
+         BetterJump();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1,groundLayer);
+        if (hit.collider != null)
+            isGrounded = true;
+        else
+            isGrounded = false;
+
         direction = (int)Input.GetAxisRaw("Horizontal");
-        if(!isDashing)
-            Move();
-   
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(direction != 0)
+            oldDirection = direction;
+        Move();
+        //DASH
+        if (Input.GetKeyDown(KeyCode.LeftShift) && _canDash)
+        {
+            _isDashing = true;
+            _canDash = false;
+            _dashingDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            _trailRen.emitting = true;
+            if (_dashingDir == Vector2.zero)
+                _dashingDir.x = oldDirection;
+            anim.SetTrigger("isDashing");
+            StartCoroutine(StopDashing());
+        }
+       
+        if (_isDashing)
+        {
+            rb.linearVelocity = new Vector2(_dashingDir.x * _dashingVelocity, _dashingDir.y * _dashingVelocity/2);
+            return;
+        }
+        else
+            rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+
+        //DASH
+        if (isGrounded)
+            _canDash = true;
+        
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !_isDashing)
         { 
             anim.SetTrigger("isJumping");
-            rb.linearVelocity = Vector2.up * jumpForce;
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
         }
-        if (Input.GetKeyDown(KeyCode.Z) && canDash)
-        {
-            Dash();
-
-        }
+      
     }
 }
